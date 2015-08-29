f3e60a275096d0aeb2fe539790897f9fcf9027ac
Peter Sobkowski <peter.sobkowski@gmail.com>
2015-08-29 14:05:49 -0400

    Adding first 3 steps

---
+++ Step 2/Files/script.js
@@ -0,0 +1,44 @@
+function dragResize(edge){
+	overwolf.windows.getCurrentWindow(function(result){
+		if (result.status=="success"){
+			overwolf.windows.dragResize(result.window.id, edge);
+		}
+	});
+};
+
+function dragMove(){
+	overwolf.windows.getCurrentWindow(function(result){
+		if (result.status=="success"){
+			overwolf.windows.dragMove(result.window.id);
+		}
+	});
+};
+
+function closeWindow(){
+	overwolf.windows.getCurrentWindow(function(result){
+		if (result.status=="success"){
+			overwolf.windows.close(result.window.id);
+		}
+	});
+};
+
+function takeScreenshot(){
+	overwolf.media.takeScreenshot(function(result){
+		if (result.status == "success"){
+			var img = document.getElementById("screenshot");
+			img.src = result.url;
+			img.onload = function() {
+				overwolf.media.shareImage(img, "Screen Shot");
+			};
+		}
+	});
+}
+
+function runTeamSpeak(){
+	overwolf.extensions.launch("lafgmhfbkjljkgoggomibmhlpijaofafbdhpjgif");
+};
+
+overwolf.profile.getCurrentUser(function(value){
+	var playerData = value; 
+	document.getElementById('usernameContainer').innerHTML = playerData.username;
+});
